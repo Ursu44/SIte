70034f0dd56d7d0d28b71590cdd4dbe70368a7bc
cosmin.aursulesei <cosmin-irimia.aursulesei@student.tuiasi.ro>
2026-09-11 07:13:05 +0300

    COmmit 7

--- src/app/components/list-section/list-section.ts
+++ src/app/components/list-section/list-section.ts
@@ -1,7 +1,6 @@
 import { Component, input, computed, afterNextRender, output, ViewChild, ElementRef } from '@angular/core';
 
 interface NewsItem {
-  date: string;
   title: string;
   description: string;
 }
@@ -25,10 +24,10 @@ export class ListSection {
   });
 
   newsItems: NewsItem[] = [
-    { date: '28 August 2026', title: 'Lansăm un nou serviciu pentru clienți', description: 'Aflați cum vă putem ajuta mai bine cu cea mai nouă soluție dedicată nevoilor dumneavoastră.' },
-    { date: '15 August 2026', title: 'Parteneriat strategic extins', description: 'Am semnat un acord important care ne va permite să oferim servicii și mai complete.' },
-    { date: '2 August 2026', title: 'Certificare nouă obținută', description: 'Un pas important pentru garantarea calității serviciilor pe care le oferim clienților noștri.' },
-    { date: '20 Iulie 2026', title: 'Deschidem o nouă sucursală', description: 'Extindem prezența noastră pentru a fi mai aproape de comunitatea pe care o deservim.' }
+    { title: 'Dobanzi avantajoase', description: 'Oferim imprumutri cu dobanzi pentru orice buzunar.' },
+    {  title: 'Depunere de fonduri', description: 'Am semnat un acord important care ne va permite să oferim servicii și mai complete.' },
+    {  title: 'Usurinta in accesarea serviciilor', description: '' },
+    {  title: 'Deschidem o nouă sucursală', description: 'Extindem prezența noastră pentru a fi mai aproape de comunitatea pe care o deservim.' }
   ];
 
    constructor() {
